19932e79b6d3aef87172a2765d7882b5b9c39778
Vadims SERGEJEVS <VSergejevs@nkteh.com>
2024-07-20 20:54:02 +0300

    menu burger

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# ui-elements-collection
+# UI Elements Collection
+
+This project is a collection of UI elements built with pure HTML, CSS, and JavaScript. It includes components like burger menu, slider, accordion, and more.
+
+## Project Structure
+
+- `index.html`: The main HTML file.
+- `scss/...`: The SCSS files for styling.
+- `js/...`: The JavaScript files for interactions.
+
+## Elements Included
+
+- Burger Menu
+- Slider (to be added)
+- Accordion (to be added)
+- Modal (to be added)
+- Tabs (to be added)
+- Tooltip (to be added)
---
+++ js/BurgerFullcreen.js
@@ -0,0 +1,22 @@
+class BurgerFullcreen{
+    constructor(burger, menu) {
+        this.burger = burger;
+        this.menu = menu;
+        this.init();
+    }
+
+    init() {
+        this.burger.addEventListener('click', this.toggleClass.bind(this))
+    }
+
+    toggleClass(event) {
+        event.preventDefault();
+        this.burger.classList.toggle('active');
+        this.menu.classList.toggle('menu-opened');
+        document.body.classList.toggle('menu-opened');
+    }
+}
+
+const burger = document.querySelector('#burger');
+const menu = document.querySelector('#menu');
+new BurgerFullcreen(burger, menu);
