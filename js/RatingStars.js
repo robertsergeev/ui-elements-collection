9cbbba5c35b44f1df51ccc0efe15607e557d3b23
robertsergeev <user210307@gmail.com>
2024-07-23 23:48:40 +0300

    Refactored: RatingStars.js

--- js/RatingStars.js
+++ js/RatingStars.js
@@ -1,50 +1,44 @@
-// TODO: create an updateStars method to reduce repetition of the same code
-
 class RatingStars{
-    constructor(stars) {
-        this.stars = [...stars];
-        this.selectedStar = 0;
-        this.init();
+    constructor(starsContainer, stars) {
+        if(starsContainer && stars) {
+            this.starsContainer = starsContainer;
+            this.stars = [...stars];
+            this.selectedStar = 0;
+            this.init();
+        }
+
     }
 
     init(){
-        const parent = this.stars[0].parentElement
-        parent.addEventListener('click', this.fill.bind(this));
-        parent.addEventListener('mouseover', this.preview.bind(this));
-        parent.addEventListener('mouseleave', this.unfill.bind(this));
+        this.starsContainer.addEventListener('click', this.fill.bind(this));
+        this.starsContainer.addEventListener('mouseover', this.preview.bind(this));
+        this.starsContainer.addEventListener('mouseleave', this.unfill.bind(this));
     }
 
     fill(event) {
         if(event.target.closest('.icon')) {
-            this.selectedStar = event.target.closest('.icon').dataset.order;
+            this.selectedStar = +event.target.closest('.icon').dataset.order;
         
-            for(let i = 0; i < this.stars.length; i++) {
-                if(i < this.selectedStar) {
-                    this.stars[i].classList.add('filled');
-                } else {
-                    this.stars[i].classList.remove('filled');
-                }
-            }
+            this.updateStars(this.selectedStar)
         }
     }
 
     preview(event) {
         if(event.target.classList.contains('icon')) {
-            let order = event.target.dataset.order;
+            let order = +event.target.dataset.order;
 
-            for(let i = 0; i < this.stars.length; i++) {
-                if(i < order) {
-                    this.stars[i].classList.add('filled');
-                } else {
-                    this.stars[i].classList.remove('filled');
-                }
-            }
+            this.updateStars(order)
+
         }
     }
 
     unfill() {
+        this.updateStars(this.selectedStar)
+    }
+
+    updateStars(breakpoint) {
         for(let i = 0; i < this.stars.length; i++) {
-            if(i < this.selectedStar) {
+            if(i < breakpoint) {
                 this.stars[i].classList.add('filled');
             } else {
                 this.stars[i].classList.remove('filled');
@@ -54,5 +48,5 @@ class RatingStars{
 }
 
 const stars = document.querySelectorAll('.rating-stars .stars svg');
-new RatingStars(stars)
-
+const starsContainer = document.querySelector('.rating-stars .stars');
+new RatingStars(starsContainer, stars);
